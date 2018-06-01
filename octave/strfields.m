a536bcd6964548dc674009299f0ed0b2758807ba
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2018-06-01 10:13:44 +0200

    added strfields octave routine: string to cell array

---
+++ octave/strfields.m
@@ -0,0 +1,27 @@
+% Copyright (C) 2015 Alberto Otero-de-la-Roza <aoterodelaroza@gmail.com>
+%
+% acp is free software: you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free
+% Software Foundation, either version 3 of the License, or (at your
+% option) any later version. See <http://www.gnu.org/licenses/>.
+%
+% The routine distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+% more details.
+
+function ac = strfields(txt)
+  %% function ac = strfields(txt)
+  %%
+  %% Separate the string txt into a cell array of fields.
+  
+  ac = {};
+  af = -1;
+  txt = strtrim(txt);
+  af = sscanf(txt,"%s","C");
+  while (!isempty(af))
+    ac = {ac{:} af};
+    txt = strtrim(txt(length(af)+1:end));
+    af = sscanf(txt,"%s","C");
+  endwhile
+end
