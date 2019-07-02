7322d3dca355a9615a3eb9bef6c77e9b29b32ed7
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2019-07-02 11:30:36 +0200

    added init_*-ph.sh to the cedar initialization

---
+++ submit/bash/cedar/qe/init_qegit-ph.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+init_qegit(){
+    cat >&3 <<EOM
+unset I_MPI_PMI_LIBRARY
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export ESPRESSO_TMPDIR=\${SLURM_TMPDIR}
+export CRITIC_HOME="/home/alberto/git/critic2"
+
+module load intel/2016
+module load openmpi
+module load fftw
+export ESPRESSO_HOME=/home/alberto/git/espresso-phwithcg
+export PATH=\$PATH:\${ESPRESSO_HOME}/bin
+A=\$ESPRESSO_HOME/bin
+
+EOM
+}
