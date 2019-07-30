170bbf3e7bdbeb2788de0923e171e701ca6cc3cf
Alberto Otero de la Roza <alberto@cedar1.cedar.computecanada.ca>
2019-07-30 07:54:02 -0700

    make postg available to psi4 submission scripts

--- submit/bash/cedar/psi4/init_.sh
+++ submit/bash/cedar/psi4/init_.sh
@@ -4,7 +4,7 @@ init_(){
     cat >&3 <<EOM
 module load cmake intel/17 python/3.7 scipy-stack boost hdf5
 
-export PATH=/home/alberto/src/psi4/bin/
+export PATH=/home/alberto/src/psi4/bin/:/home/alberto/git/postg:\$PATH
 export PSIDATADIR=/home/alberto/src/psi4/share/psi4/
 export PSI_SCRATCH=\${SLURM_TMPDIR}
 export LIBRARY_PATH=/home/alberto/src/psi4/lib/:\$LIBRARY_PATH
