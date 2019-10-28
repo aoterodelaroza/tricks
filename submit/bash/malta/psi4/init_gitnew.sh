0746f26b24eb005aa6e69c5f20ca32ccc93239ab
Alberto Otero de la Roza <alberto@malta.quimica.uniovi.es>
2019-10-28 20:50:10 +0100

    update psi4 submit script, malta

--- submit/bash/malta/psi4/init_gitnew.sh
+++ submit/bash/malta/psi4/init_gitnew.sh
@@ -8,7 +8,7 @@ module load intel/15.3
 module load mkl/15.3 
 module load postg/master 
 
-export PSI_SCRATCH=${TMPDIR}
+export PSI_SCRATCH=\${TMPDIR}
 export PATH=/opt/uovi/alberto/psi4-new/bin/:\${PATH}
 export LD_LIBRARY_PATH=/opt/uovi/alberto/psi4-new/lib:\${LD_LIBRARY_PATH}
 export LD_RUN_PATH=/opt/uovi/alberto/psi4-new/lib:\${LD_RUN_PATH}
