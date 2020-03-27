bc4290c6d44f5c0e668aeb1d64fead8431babb8e
Alberto Otero de la Roza <alberto@malta.quimica.uniovi.es>
2020-03-28 00:24:42 +0100

    update the run_thermo script for malta/qe

--- submit/bash/malta/qe/run_thermo.sh
+++ submit/bash/malta/qe/run_thermo.sh
@@ -3,7 +3,7 @@
 run_thermo(){
     cat >&3 <<EOM
 ##xx## Use thermo_pw.x
-/opt/openmpi/1.8.4/bin/mpirun --mca btl_sm_use_knem 0 --mca btl_vader_single_copy_mechanism none -np ${ncpu} \$ESPRESSO_HOME/bin/thermo_pw.x < ${i}.scf.in > ${i}.scf.out
+/opt/openmpi/1.8.4/bin/mpirun --mca btl_sm_use_knem 0 --mca btl_vader_single_copy_mechanism none -np ${ncpu} \$ESPRESSO_HOME/bin/thermo_pw.x -i ${i}.scf.in > ${i}.scf.out
 
 EOM
 }
