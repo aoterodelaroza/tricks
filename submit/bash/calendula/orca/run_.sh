33925dc120925a3c3edc8061fb2da5cb4d9f042e
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2020-11-25 17:57:19 +0100

    updated calendula scripts

--- submit/bash/calendula/orca/run_.sh
+++ submit/bash/calendula/orca/run_.sh
@@ -2,12 +2,17 @@
 
 run_(){
 	cat >&3 <<EOM
+newdir=\$(mktemp -d ${i}_XXXXXXXX)
+cp -f ${i}.inp \$newdir
+cd \$newdir
 /soft/calendula2/sandybridge/ORCA/4.2.0/orca ${i}.inp > ${i}.out ${AMP}
 orca_2mkl ${i} -molden
+orca_2mkl ${i} 
 mv ${i}.molden.input ${i}.molden
-xz ${i}.gbw ${i}.molden ${i}.wfx
-rm -f *.tmp* *.txt *.prop *.wfn
-
+xz ${i}.gbw ${i}.molden ${i}.wfx ${i}.wfn
+mv ${i}.inp ${i}.out *.xz ..
+cd ..
+rm -r \$newdir
 
 EOM
 }
