c1b2f281522b5ed5b6698c3554c4f8b26a9e2787
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2020-12-02 18:35:25 +0000

    rename init_g16

---
+++ submit/bash/malta/gaussian/init_g16.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+init_g16(){
+    cat >&3 <<EOM
+## g16
+export g16root=/home/alberto/src
+. \$g16root/g16/bsd/g16.profile
+export GAUSS_SCRDIR=\$TMPDIR
+
+## postg
+export POSTG_HOME=/opt/uovi/alberto/postg
+export PATH=\${PATH}:\${POSTG_HOME}
+export OMP_NUM_THREADS=${ncpu}
+
+## name for the run routine
+export GAUPROGRAM=g16
+
+EOM
+}
