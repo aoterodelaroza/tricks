a76edbad35778d3a882fd60a48d9b5c9a089eb33
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2021-02-11 13:49:44 +0100

    update the cedar qe submission scripts to the new environment

--- submit/bash/cedar/qe/init_qegit-ph.sh
+++ submit/bash/cedar/qe/init_qegit-ph.sh
@@ -8,9 +8,13 @@ export MKL_NUM_THREADS=1
 export ESPRESSO_TMPDIR=\${SLURM_TMPDIR}
 export CRITIC_HOME="/home/alberto/git/critic2"
 
-module load intel/2016
-module load openmpi
-module load fftw
+module --force purge
+module load StdEnv/2016.4
+module load intel/2017.5
+module load imkl/2017.1.132
+module load fftw-mpi/3.3.6
+module load libxc
+module load openmpi/2.1.1
 export ESPRESSO_HOME=/home/alberto/git/espresso-phwithcg
 export PATH=\$PATH:\${ESPRESSO_HOME}/bin
 A=\$ESPRESSO_HOME/bin
