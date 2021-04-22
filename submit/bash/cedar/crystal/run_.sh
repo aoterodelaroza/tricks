ee2c34f1a08a39ce89b3de8bcbf19b301857f17c
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2021-04-22 17:15:09 +0200

    update the run script for cedar/crystal submission

--- submit/bash/cedar/crystal/run_.sh
+++ submit/bash/cedar/crystal/run_.sh
@@ -2,9 +2,14 @@
 
 run_(){
 	cat >&3 <<EOM
-cp -f ${i}.d12 INPUT
-## cp -f ../../empty/${i}/fort.9 fort.20
+
+cp -f ${i}.fort.34 \${SLURM_TMPDIR}/fort.34
+cp -f ${i}.d12 \${SLURM_TMPDIR}/
+cp -f ${i}.d12 \${SLURM_TMPDIR}/INPUT
+cd \${SLURM_TMPDIR}
 mpirun -np ${ncpu} ~/src/crystal17/bin/Linux-ifort/Pcrystal < ${i}.d12 >& ${i}.out ${AMP}
+cp -f ${i}.out $(pwd)/${i}
+rm -f *.34 *.d12 *.out
 
 EOM
 }
