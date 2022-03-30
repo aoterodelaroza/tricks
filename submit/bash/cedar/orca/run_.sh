46dcc8f1b348139cd1717009b6bf3fd5808aaad5
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-03-30 16:31:40 +0200

    update orca scripts in cedar

--- submit/bash/cedar/orca/run_.sh
+++ submit/bash/cedar/orca/run_.sh
@@ -2,7 +2,10 @@
 
 run_(){
 	cat >&3 <<EOM
+cp -f ${i}.inp \${SLURM_TMPDIR}/
+cd \${SLURM_TMPDIR}/
 \$(which orca) ${i}.inp 2>&1 > ${i}.out ${AMP}
+mv ${i}.out ${i}.wfn ${i}.wfx $(pwd)
 # xz ${i}.gbw
 
 EOM
