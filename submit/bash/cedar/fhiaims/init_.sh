65214c33ecb9880f79477c68127c11d77fb7298d
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-04-04 11:57:24 +0200

    update cedar/fhiaims scripts

--- submit/bash/cedar/fhiaims/init_.sh
+++ submit/bash/cedar/fhiaims/init_.sh
@@ -4,7 +4,7 @@ init_(){
     cat >&3 <<EOM
 module purge
 module load StdEnv/2020
-module load intel/2020.1.217 intelmpi/2019.7.217 imkl/2020.1.217
+module load intel/2020.1.217 imkl/2020.1.217 openmpi/4.0.3
 module load libxc/5.1.3
 
 export OMP_NUM_THREADS=1
--- submit/bash/cedar/fhiaims/run_.sh
+++ submit/bash/cedar/fhiaims/run_.sh
@@ -3,7 +3,7 @@
 run_(){
 	cat >&3 <<EOM
 
-srun /home/alberto/src/FHIaims_XDM/build/aims.210513.scalapack.mpi.x < /dev/null > ${i}.out
+mpiexec -n \$SLURM_NTASKS /home/alberto/src/FHIaims_XDM/build/aims.210513.scalapack.mpi.x < /dev/null > ${i}.out
 
 EOM
 }
