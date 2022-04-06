e5b4a01fce73e34c13d77a3392b10a48e30f127c
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-04-06 09:23:17 +0200

    do not move orca input to the scratch in cedar submission

--- submit/bash/cedar/orca/run_.sh
+++ submit/bash/cedar/orca/run_.sh
@@ -2,10 +2,7 @@
 
 run_(){
 	cat >&3 <<EOM
-cp -f ${i}.inp \${SLURM_TMPDIR}/
-cd \${SLURM_TMPDIR}/
 \$(which orca) ${i}.inp 2>&1 > ${i}.out ${AMP}
-mv ${i}.out ${i}.wfn ${i}.wfx $(pwd)
 # xz ${i}.gbw
 
 EOM
