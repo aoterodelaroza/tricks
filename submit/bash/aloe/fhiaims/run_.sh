26918e5abb945133fde114eaece1b251060d7911
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-04-07 12:17:40 +0200

    update aloe/fhiaims scripts

--- submit/bash/aloe/fhiaims/run_.sh
+++ submit/bash/aloe/fhiaims/run_.sh
@@ -3,7 +3,7 @@
 run_(){
 	cat >&3 <<EOM
 
-mpirun /opt/software/FHIaims_XDM/bin/aims.210513.mpi.x < /dev/null > ${i}.out
+mpirun /opt/software/FHIaims_XDM/bin/aims.210513.scalapack.mpi.x < /dev/null > ${i}.out
 
 EOM
 }
