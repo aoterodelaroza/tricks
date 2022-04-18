61ebcb4c005d9437e1fef9e0b18e4fe3c5f19dce
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-04-18 12:04:38 +0200

    update cedar/fhiaims

--- submit/bash/cedar/fhiaims/run_.sh
+++ submit/bash/cedar/fhiaims/run_.sh
@@ -3,7 +3,7 @@
 run_(){
 	cat >&3 <<EOM
 
-mpiexec -n \$SLURM_NTASKS /home/alberto/src/FHIaims_XDM/build/aims.210513.scalapack.mpi.x < /dev/null > ${i}.out
+mpiexec -n \$SLURM_NTASKS /home/alberto/src/FHIaims_XDM/build/aims.210513.mpi.x < /dev/null > ${i}.out
 
 EOM
 }
