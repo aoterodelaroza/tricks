c17a10d8ef86c4607a33dd02e272fff75f330caa
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-05-11 18:19:35 +0200

    update orca environment on cedar

--- submit/bash/cedar/orca/init_.sh
+++ submit/bash/cedar/orca/init_.sh
@@ -2,6 +2,7 @@
 
 init_(){
     cat >&3 <<EOM
+module load StdEnv/2018.3
 module load openmpi
 export PATH=/home/alberto/src/orca_5_0_3/:\$PATH
 export LD_LIBRARY_PATH=/home/alberto/src/orca_5_0_3/:\$LD_LIBRARY_PATH
