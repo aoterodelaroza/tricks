dbe896aa84a8106921e39beff24348863e8d83ab
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2022-09-19 12:56:12 +0200

    updated submission scripts fhiaims

--- submit/bash/aloe/fhiaims/run_basic.sh
+++ submit/bash/aloe/fhiaims/run_basic.sh
@@ -3,7 +3,7 @@
 run_basic(){
 	cat >&3 <<EOM
 
-mpirun /opt/software/FHIaims_XDM/bin/aims.210513.mpi.x < /dev/null > ${i}.out
+mpirun /opt/software/FHIaims-220915_clean/build/aims.220915.mpi.x < /dev/null > ${i}.out
 
 EOM
 }
