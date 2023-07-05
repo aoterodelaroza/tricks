914967fa79098cc94aa262f3e19e4cf61f766656
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-07-05 14:53:57 +0200

    more tweaks to the sockeye/orca scripts

--- submit/bash/sockeye/orca/run_.sh
+++ submit/bash/sockeye/orca/run_.sh
@@ -4,7 +4,7 @@ run_(){
 	cat >&3 <<EOM
 cp -f ${i}.inp \$TMPDIR
 cd \$TMPDIR
-/home/aoterode/src/orca_5_0_3/orca ${i}.inp > ${i}.out ${AMP}
+/home/aoterode/src/orca_5_0_3/orca ${i}.inp > $(pwd)/${i}.out ${AMP}
 
 EOM
 }
