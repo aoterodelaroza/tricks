6421307366f7c6b6da83babd487894f7e188bcba
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-07-05 14:36:40 +0200

    update sockeye header files

--- submit/bash/sockeye/header_mpi.sh
+++ submit/bash/sockeye/header_mpi.sh
@@ -5,9 +5,9 @@ header_mpi(){
 #! /bin/bash
 #PBS -l walltime=${walltime},select=1:ncpus=${ncpu}:mpiprocs=${ncpu}:mem=${mem}
 #PBS -N ${prefix}-${i}
-#PBS -A dri-dilabio
+#PBS -A st-dilabio-1
 #PBS -j eo
-#PBS -e $(pwd)/${i}.err 
+#PBS -e $(pwd)/${i}.err
 #PBS ${sbatchadd}
 
 EOM
