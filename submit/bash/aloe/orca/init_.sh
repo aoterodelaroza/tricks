6db78cbbc34d891bba6a53cb163b39a062bf6764
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-08-29 17:43:43 +0200

    update orca version

--- submit/bash/aloe/orca/init_.sh
+++ submit/bash/aloe/orca/init_.sh
@@ -2,7 +2,7 @@
 
 init_(){
     cat >&3 <<EOM
-export PATH=/opt/software/orca_5_0_3/:\$PATH
-export LD_LIBRARY_PATH=/opt/software/orca_5_0_3/:\$LD_LIBRARY_PATH
+export PATH=/opt/software/orca_5_0_4/:\$PATH
+export LD_LIBRARY_PATH=/opt/software/orca_5_0_4/:\$LD_LIBRARY_PATH
 EOM
 }
--- submit/bash/cedar/orca/init_.sh
+++ submit/bash/cedar/orca/init_.sh
@@ -4,8 +4,8 @@ init_(){
     cat >&3 <<EOM
 module load StdEnv/2018.3
 module load openmpi
-export PATH=/home/alberto/src/orca_5_0_3/:\$PATH
-export LD_LIBRARY_PATH=/home/alberto/src/orca_5_0_3/:\$LD_LIBRARY_PATH
+export PATH=/home/alberto/src/orca_5_0_4/:\$PATH
+export LD_LIBRARY_PATH=/home/alberto/src/orca_5_0_4/:\$LD_LIBRARY_PATH
 
 EOM
 }
