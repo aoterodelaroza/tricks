2ff26eb5da1c07f11935cbe06d1924226195dc5a
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-11-05 12:35:14 +0100

    update sockeye/orca script

--- submit/bash/sockeye/orca/init_.sh
+++ submit/bash/sockeye/orca/init_.sh
@@ -7,6 +7,7 @@ module load openmpi/4.1.1-cuda11-3
 
 ## orca
 export LD_LIBRARY_PATH=/home/aoterode/src/orca_5_0_3:\$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=\$OPENMPI_ROOT/lib:\$LD_LIBRARY_PATH
 
 EOM
 }
