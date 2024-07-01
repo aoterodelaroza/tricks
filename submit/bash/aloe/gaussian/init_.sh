a97db3de5f8b56246f0541d2d43ff33aa55a31fc
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-07-01 22:52:16 +0200

    update aloe/gaussian scripts

--- submit/bash/aloe/gaussian/init_.sh
+++ submit/bash/aloe/gaussian/init_.sh
@@ -2,6 +2,15 @@
 
 init_(){
     cat >&3 <<EOM
+if [ -f /etc/sie_ladon ] ; then
+    export g16root="/opt/software/g16A-sie_ladon"
+elif [ -f /etc/sr630 ] ; then
+    export g16root="/opt/software/g16A-sr630"
+else
+    echo "could not found ID file in /etc"
+    exit
+fi
+echo "g16root = \$g16root"
 export g16root="/opt/software"
 . \$g16root/g16/bsd/g16.profile
 export GAUSS_SCRDIR=\${SLURM_TMPDIR}
