0aef363055b88b9fea1e4f017d25a7e6ab3dc966
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-07-09 16:37:32 +0200

    update sockeye/qe scripts

--- submit/bash/sockeye/qe/init_.sh
+++ submit/bash/sockeye/qe/init_.sh
@@ -3,14 +3,15 @@
 init_(){
     cat >&3 <<EOM
 source /etc/profile
-module load gcc/9.1.0
-module load openmpi/3.1.4
+module load gcc/9.4.0
+module load openmpi/4.1.1-cuda11-3
+module load intel-mkl/2020.4.304
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export CRITIC_HOME=/home/aoterode/git/critic2
 export ESPRESSO_TMPDIR=\${SLURM_TMPDIR}
-export ESPRESSO_HOME=/home/aoterode/git/espresso
+export ESPRESSO_HOME=/home/aoterode/src/espresso-7.3.1
 export PATH=\$PATH:\${ESPRESSO_HOME}/bin
 A=\$ESPRESSO_HOME/bin
 
