89fc2fc53b92df88cffc0bfce5ae1d03b7153438
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-07-24 17:21:29 +0200

    update aloe/crystal scripts

--- submit/bash/aloe/crystal/run_.sh
+++ submit/bash/aloe/crystal/run_.sh
@@ -6,10 +6,16 @@ export PMIX_MCA_psec=^munge
 export PMIX_MCA_gds=^shmem2
 
 cp -f ${i}.fort.34 fort.34 2>&1 > /dev/null
-cp -f ${i}.d12 INPUT
 
+cp -f ${i}.d12 INPUT
 srun /opt/software/crystal17/bin/Linux-gfortran_openmpi/v1.0.2/Pcrystal < ${i}.d12 >& ${i}.out
 mv fort.34 ${i}.end.fort.34
+
+# cp -f ${i}.d3 INPUT
+# srun /opt/software/crystal17/bin/Linux-gfortran_openmpi/v1.0.2/Pproperties < ${i}.d3 >& ${i}.d3out
+
+# cp fort.25 ${i}.f25
+# /opt/software/crystal17/crgra2006/band06 ${i}
 
 EOM
 }
