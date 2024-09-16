e0484c75c56c84a19594112e0aa42b9022580fea
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-09-16 11:58:45 +0200

    update mn5/qe

--- submit/bash/mn5/qe/run_opt.sh
+++ submit/bash/mn5/qe/run_opt.sh
@@ -6,13 +6,13 @@ run_opt(){
 cp -f ${i}.scf.in ${i}.scf.in1
 cp -f ${i}.scf.out ${i}.scf.out1
 
-/home/alberto/bin/pwout2in.awk ${i}.scf.out ${i}.scf.in > bleh.in
+~/bin/pwout2in.awk ${i}.scf.out ${i}.scf.in > bleh.in
 mv bleh.in ${i}.scf.in
 srun  pw.x < ${i}.scf.in > ${i}.scf.out
 cp -f ${i}.scf.in ${i}.scf.in2
 cp -f ${i}.scf.out ${i}.scf.out2
 
-/home/alberto/bin/pwout2in.awk ${i}.scf.out ${i}.scf.in > bleh.in
+~/bin/pwout2in.awk ${i}.scf.out ${i}.scf.in > bleh.in
 mv bleh.in ${i}.scf.in
 srun  pw.x < ${i}.scf.in > ${i}.scf.out
 cp -f ${i}.scf.in ${i}.scf.in3
