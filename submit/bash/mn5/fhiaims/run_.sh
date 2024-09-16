1a0022d451adb25e5c32d45841bc6293a60a64fe
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-09-16 15:10:50 +0200

    update mn5/fhiaims

--- submit/bash/mn5/fhiaims/run_.sh
+++ submit/bash/mn5/fhiaims/run_.sh
@@ -2,7 +2,7 @@
 
 run_(){
 	cat >&3 <<EOM
-time srun /home/udo/udo035583/project/FHIaims/build/aims.230214.scalapack.mpi.x < /dev/null > ${i}.out
+time srun /home/udo/udo035583/project/FHIaims-xdm-stable/build/aims.240507.scalapack.mpi.x < /dev/null > ${i}.out
 
 EOM
 }
