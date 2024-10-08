c8f8fb41a10f17fa8f08c84041f741127a3607c6
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-10-08 22:06:02 +0200

    update sockeye qe scripts

--- submit/bash/sockeye/qe/init_.sh
+++ submit/bash/sockeye/qe/init_.sh
@@ -3,16 +3,14 @@
 init_(){
     cat >&3 <<EOM
 source /etc/profile
-module load intel-oneapi-compilers
-module load intel-mkl
-module load openmpi/4.1.1-cuda11-3
-export LD_LIBRARY_PATH=/arc/software/spack-2024/opt/spack/linux-rocky9-skylake_avx512/intel-2023.2.0/fftw-3.3.10-tpy2nilylq6rdgnrg3ealkw4kh2wt33f/lib/:\$LD_LIBRARY_PATH
+module load intel-oneapi-compilers/2023.1.0
+module load openmpi/4.1.6-cuda12-4 cmake intel-mkl
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export CRITIC_HOME=/home/aoterode/git/critic2
 export ESPRESSO_TMPDIR=\${SLURM_TMPDIR}
-export ESPRESSO_HOME=/home/aoterode/src/espresso-7.3.1
+export ESPRESSO_HOME=/home/aoterode/src/espresso-6.5_thermo
 export PATH=\$PATH:\${ESPRESSO_HOME}/bin
 A=\$ESPRESSO_HOME/bin
 
