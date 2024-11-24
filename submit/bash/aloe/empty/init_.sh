855349038494b068ebb87ebbf70fe1a5bb0937ef
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-11-24 18:42:55 +0100

    fix aloe/empty init

--- submit/bash/aloe/empty/init_.sh
+++ submit/bash/aloe/empty/init_.sh
@@ -2,13 +2,9 @@
 
 init_(){
     cat >&3 <<EOM
-module purge
-module load StdEnv/2020
-
-export OMP_NUM_THREADS=1
+unset I_MPI_PMI_LIBRARY
+export OMP_NUM_THREADS=${ncpu}
 export MKL_NUM_THREADS=1
-export MKL_DYNAMIC=FALSE
-ulimit -s unlimited
 
 EOM
 }
