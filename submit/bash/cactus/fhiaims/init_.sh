3f2ea9f38282ab8865951463e87d3907f25723a0
alberto <alberto@localhost.localdomain>
2025-06-16 10:59:04 +0200

    update cactus/fhiaims and cactus/qe

--- submit/bash/cactus/fhiaims/init_.sh
+++ submit/bash/cactus/fhiaims/init_.sh
@@ -8,7 +8,11 @@ export MKL_DYNAMIC=FALSE
 export CRITIC_HOME="/home/alberto/git/critic2"
 ulimit -s unlimited
 
-export FHIAIMS=/opt/software/FHIaims/bin/aims.241018.mpi.x
+. /opt/software/intel/oneapi/setvars.sh
+export PATH=/opt/software/openmpi-4.1.6_intel/bin/:\$PATH
+export LD_LIBRARY_PATH=/opt/software/openmpi-4.1.6_intel/lib/:\$LD_LIBRARY_PATH
+
+export FHIAIMS=/opt/software/FHIaims/bin/aims.241018.scalapack.mpi.x
 
 EOM
 }
--- submit/bash/cactus/qe/init_.sh
+++ submit/bash/cactus/qe/init_.sh
@@ -11,5 +11,10 @@ export CRITIC_HOME="/home/alberto/git/critic2"
 export ESPRESSO_HOME=/opt/software/espresso-6.5_thermo
 A=\${ESPRESSO_HOME}/bin/
 
+. /opt/software/intel/oneapi/setvars.sh
+export PATH=/opt/software/openmpi-4.1.6_intel/bin/:\$PATH
+export LD_LIBRARY_PATH=/opt/software/openmpi-4.1.6_intel/lib/:\$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/software/fftw-3.3.10_intel/lib/:\$LD_LIBRARY_PATH
+
 EOM
 }
