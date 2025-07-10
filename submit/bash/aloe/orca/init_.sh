700934a0d888e510467b7485a93d30f988381970
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2025-07-10 14:07:52 +0200

    update aloe/orca

--- submit/bash/aloe/orca/init_.sh
+++ submit/bash/aloe/orca/init_.sh
@@ -2,7 +2,7 @@
 
 init_(){
     cat >&3 <<EOM
-export PATH=/opt/software/orca_5_0_4/:\$PATH
-export LD_LIBRARY_PATH=/opt/software/orca_5_0_4/:\$LD_LIBRARY_PATH
+export PATH=/opt/software/orca_6_1_0_avx2/:\$PATH
+export LD_LIBRARY_PATH=/opt/software/orca_6_1_0_avx2/:\$LD_LIBRARY_PATH
 EOM
 }
