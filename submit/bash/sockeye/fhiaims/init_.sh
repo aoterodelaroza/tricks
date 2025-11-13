c5c6bae927744ef60aba3592f6e00ec3d39d460c
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2025-11-13 15:16:58 +0100

    update sockeye/fhiaims script

--- submit/bash/sockeye/fhiaims/init_.sh
+++ submit/bash/sockeye/fhiaims/init_.sh
@@ -3,8 +3,9 @@
 init_(){
     cat >&3 <<EOM
 source /etc/profile
-module load intel-oneapi-compilers/2023.1.0
-module load openmpi/4.1.6-cuda12-4 cmake intel-mkl
+module load gcc/9.4.0
+module load openmpi/4.1.1-cuda11-3
+module load intel-mkl/2020.4.304
 
 export UCX_WARN_UNUSED_ENV_VARS=n
 export OMP_NUM_THREADS=1
@@ -13,7 +14,8 @@ export MKL_DYNAMIC=FALSE
 ulimit -s unlimited
 
 ## FHIBIN=/home/aoterode/src/FHIaims-xdm-stable/build/aims.240507.mpi.x
-FHIBIN=/home/aoterode/src/FHIaims-241018/build/aims.241018.mpi.x
+## FHIBIN=/home/aoterode/src/FHIaims-241018/build/aims.241018.mpi.x
+FHIBIN=/home/aoterode/src/FHIaims/build/aims.251014.scalapack.mpi.x
 
 EOM
 }
