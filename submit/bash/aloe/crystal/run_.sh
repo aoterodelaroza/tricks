1dce506b387ed8667603d867829e68cbbc249563
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2026-03-25 12:26:34 +0100

    save DAT files when running crystal

--- submit/bash/aloe/crystal/run_.sh
+++ submit/bash/aloe/crystal/run_.sh
@@ -9,6 +9,7 @@ cp -f ${i}.fort.34 fort.34 2>&1 > /dev/null
 cp -f ${i}.d12 INPUT
 /usr/bin/mpirun /opt/software/crystal17/bin/Linux-gfortran_openmpi/v1.0.2/Pcrystal < ${i}.d12 >& $(pwd)/${i}/${i}.out
 mv fort.34 $(pwd)/${i}/${i}.end.fort.34
+mv *.DAT $(pwd)/${i}/
 
 # cp -f ${i}.d3 INPUT
 # /usr/bin/mpirun /opt/software/crystal17/bin/Linux-gfortran_openmpi/v1.0.2/Pproperties < ${i}.d3 >& $(pwd)/${i}/${i}.d3out
